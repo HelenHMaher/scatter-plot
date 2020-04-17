e5080bf22e71560cf52c03f289d07804003ef4bf
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-17 12:01:26 +0200

    fetch data and link and working environment

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ const svgContainer = d3
   .append("svg")
   .attr("width", width + inPadding + outPadding)
   .attr("height", height + inPadding + outPadding);
+
+fetch(
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+)
+  .then((response) => response.json())
+  .then((dataset) => {
+    console.log(dataset);
+  });
